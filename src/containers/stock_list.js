5da2538d2c443d346d6b5aa235dc95b3671a2d68
Virgil Xu <virgilxu89@gmail.com>
2017-12-10 14:52:23 -0800

    adding high charts

--- src/containers/stock_list.js
+++ src/containers/stock_list.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Highcharts from 'highcharts/highstock';
 
 class StockList extends Component {
-  renderStock() {
+
+  renderStockList() {
     if (this.props.stock.length > 0) {
-      var timeSeries = this.props.stock[0]['Time Series (Daily)'];
-      return Object.keys(timeSeries).map((key, i) => {
-        return <div key={i}>{timeSeries[key]['1. open']}</div>
+      return this.props.stock.map((stock, i) => {
+        var newClose = stock['Time Series (Daily)'][Object.keys(stock['Time Series (Daily)'])[0]]['4. close'];
+        var oldClose = stock['Time Series (Daily)'][Object.keys(stock['Time Series (Daily)'])[1]]['4. close'];
+        return (
+          <div id='Stock-price'key={i}>
+            <div>{stock['Meta Data']['2. Symbol']}</div>
+            <div>{Number(newClose, 10).toFixed(2)}</div>
+            <div>{(100*(newClose - oldClose)/oldClose).toFixed(2) +'%'}</div>
+          </div>
+
+        )
       })
     }
+  }
+
+  renderChart() {
+    if(this.props.stock.length > 0) {
+      var timeSeries = this.props.stock[0]['Time Series (Daily)'];
+      var data = Object.keys(timeSeries).map((date, i) => {
+        return [new Date(date).getTime(), Number(timeSeries[date]['1. open'])];
+      })
+      data.reverse();
+      console.log(data);
+      Highcharts.stockChart('Chart', {
+        rangeSelector: {
+          selected: 1
+        },
+        series:[{
+          name: this.props.stock[0]['Meta Data']['2. Symbol'],
+          data: data
+        }]
+      });
+    }
+
   }
 
   render() {
     if (this.props.stock) {
       return (
-        <div>{(this.renderStock())}</div>
+        <div className='list-chart'>
+          <div id='Stock-list'>{(this.renderStockList())}</div>
+          <div>{(this.renderChart())}</div>
+          <div id='Chart'></div>
+        </div>
+
       )
     }
   }
 }
 
 function mapStateToProps(state) {
+  console.log(state);
   return { stock: state.stock};
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
+import './style/main.scss';
 import App from './components/app';
 import reducers from './reducers';
 
